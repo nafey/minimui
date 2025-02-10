6950647e4b6c31ad1a1642bb513cc99715c672e5
ynafey <ynafey@gmail.com>
2025-02-10 00:01:47 -0500

    Collapse works

--- src/CollapseButton.jsx
+++ src/CollapseButton.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 import { ChevronLeft } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 
-export default function CollapseButton({ onClick }) {
+export default function CollapseButton({ onClick, left }) {
   return (
     <div className="flex justify-start" onClick={onClick}>
       <div className="flex items-center justify-center w-8 h-8 hover:bg-neutral-800 rounded-lg cursor-pointer">
-        <ChevronLeft color="white" size={18} />
+        {left ? (
+          <ChevronLeft color="white" size={18} />
+        ) : (
+          <ChevronRight color="white" size={18} />
+        )}
       </div>
     </div>
   );
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
     <div className="w-64 h-full text-left text-white p-4 border-r border-neutral-700 border-1 ">
       <h2 className="flex text-lg justify-between">
         <div className="mr-16">Minimalytics</div>
-        <CollapseButton onClick={onClick} />
+        <CollapseButton onClick={onClick} left={show} />
       </h2>
       <ul className="mt-4">
         <li className="py-2 cursor-pointer">Home</li>
@@ -26,7 +26,11 @@ const Sidebar = () => {
     </div>
   );
 
-  const closed = <CollapseButton onClick={onClick} />;
+  const closed = (
+    <div className="p-4">
+      <CollapseButton onClick={onClick} left={show} />
+    </div>
+  );
 
   if (show) {
     return open;
